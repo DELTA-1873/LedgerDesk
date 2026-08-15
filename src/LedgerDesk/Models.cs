ff85ed1a808c12ceedec45739ff6d4bb937f7e99
DELTA-1873 <forthefirsttime-V@outlook.com>
2026-08-15 11:12:46 +0800

    revert account system and clarify total assets

--- src/LedgerDesk/Models.cs
+++ src/LedgerDesk/Models.cs
@@ -9,8 +9,6 @@ public class Entry : INotifyPropertyChanged
     public DateTime Date { get; set; } = DateTime.Today;
     public string Category { get; set; } = "其他支出";
     public string Account { get; set; } = "微信";
-    public string ToAccount { get; set; } = "";
-    public decimal Fee { get; set; }
     public string Party { get; set; } = "";
     public string Status { get; set; } = "已结清";
     public DateTime? DueDate { get; set; }
@@ -24,22 +22,9 @@ public class Entry : INotifyPropertyChanged
     public event PropertyChangedEventHandler? PropertyChanged;
     public void Changed([CallerMemberName] string? name=null)=>PropertyChanged?.Invoke(this,new(name));
 }
-public class AccountDefinition
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public string Name { get; set; } = "新账户";
-    public string Type { get; set; } = "其他";
-    public decimal OpeningBalance { get; set; }
-    public DateTime StartDate { get; set; } = DateTime.Today;
-    public bool IncludeInTotal { get; set; } = true;
-    public bool IsArchived { get; set; }
-    public int SortOrder { get; set; }
-    public string Color { get; set; } = "#176B51";
-}
 public class LedgerData
 {
     public List<Entry> Records { get; set; } = [];
-    public List<AccountDefinition> Accounts { get; set; } = [];
     public Dictionary<string,List<string>> Categories { get; set; } = new()
     {
         ["收入"]=["工资","奖金","副业","报销","其他收入"], ["支出"]=["餐饮","购物","交通","住房","医疗","娱乐","教育","其他支出"],
